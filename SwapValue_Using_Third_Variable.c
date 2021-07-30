47ba67c2c80492a4a65123aa69fcb2b5b01d4b85
GreasyGorilla <88182781+GreasyGorilla@users.noreply.github.com>
2021-07-30 13:16:53 +0530

    Create SwapValue_Using_Third_Variable.c

---
+++ SwapValue_Using_Third_Variable.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter two no :\n");
+    scanf("%d%d",&a,&b);
+    c=a;
+    a=b;
+    b=c;
+    printf("After swapping value of a = %d\n b =%d",a,b);
+    return 0;
+}
